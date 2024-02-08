b05c9faf38611c9716dfea3d39c891428c4a777c
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-02-08 14:50:15 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
     basic.pause(2000)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
 })
 input.onButtonPressed(Button.B, function () {
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
     basic.pause(2000)
     Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
 })
